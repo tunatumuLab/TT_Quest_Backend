add03fa4526953d7dc4178c76832ad2ca139d3e9
tunatumuLab <149754352+tunatumuLab@users.noreply.github.com>
2023-11-04 01:03:11 +0900

    Update call_openai.py

--- call_openai.py
+++ call_openai.py
@@ -25,35 +25,39 @@ def gen_stage_list(dungeon_name):
 
 # 複数の、問題・解答の組を生成（入力： カテゴリ名）
 def gen_question_list(dungeon_name, stage_name):
+  _messages=[
+    {"role":"system", "content":'''The output should be a markdown code snippet formatted in the following schema; only values must be Japanese:
+    # Please do not include anything other than JSON in your answer.
+
+    \`\`\`json
+    [
+      {
+         question: string,
+         choices: array of string,   // four candidates of the answer.
+         answer: int,   // index of the choices (start with zero)
+         difficulity: string   // high, mid, low
+      },
+      {
+         question: string,
+         choices: array of string,   // four candidates of the answer.
+         answer: int,   // index of the choices (start with zero)
+         difficulity: string   // high, mid, low
+      }
+    ]
+    \`\`\`
+    '''},
+    {"role": "user", "content": "dummy"}
+  ]
+  # 代入
+  txt = "{0}の試験範囲のうち、{1}の理解度を問う問題を3段階の難易度で分類し、それぞれの難易度における「question, choices, answer」の組を5つずつ出力してください".format(dungeon_name,stage_name)
+  _messages[1]["content"] = txt
+  
   response = openai.ChatCompletion.create(
     model="gpt-4",
 #    model="gpt-3.5-turbo",
-    messages=[
-#        {"role": "system", "content": "JSON形式で出力お願いします。選択肢は配列形式、正解は配列のインデックス番号、各問題オブジェクトに難易度のキーを付けてください"},
-      {"role":"system", "content":'''The output should be a markdown code snippet formatted in the following schema; only values must be Japanese:
-      # Please do not include anything other than JSON in your answer.
-
-      \`\`\`json
-      [
-        {
-           question: string,
-           choices: array of string,   // candidates of the answer.
-           answer: int,   // index of the choices (start with zero)
-           difficulity: string   // high, mid, low
-        },
-        {
-           question: string,
-           choices: array of string,   // candidates of the answer.
-           answer: int,   // index of the choices (start with zero)
-           difficulity: string   // high, mid, low
-        }
-      ]
-      \`\`\`
-      '''},
-      {"role": "user", "content": "ITパスポートの試験範囲のうち、ネットワーク管理で出題されるような問題を３段階の難易度で分類し、それぞれの難易度における「例題と４択の回答案、そして正解」を組みとして、5つずつ出力してください"}
-#        {"role": "user", "content": "ITパスポートで出題されるような問題を３段階の難易度で分類し、それぞれの難易度における「例題と４択の回答案、そして正解」を組みとして、2つずつ出力してください"}
-    ]
+    messages=_messages
   )
+  
   _list_question = response["choices"][0]["message"]["content"]["questions"]
   
   return _list_question
