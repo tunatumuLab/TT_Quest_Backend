10ca493d17393503a334e8e299a31109ce8720cd
tunatumuLab <149754352+tunatumuLab@users.noreply.github.com>
2023-11-03 18:30:49 +0900

    json対応

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel  # リクエストbodyを定義するために必要
+from typing import List  # ネストされたBodyを定義するために必要
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-  return {"message": "Hello, World"}
 
+# リクエストbodyを定義
+class User(BaseModel):
+    user_id: int
+    name: str
+
+
+# シンプルなJSON Bodyの受け取り
+@app.post("/user/")
+# 上で定義したUserモデルのリクエストbodyをuserで受け取る
+# user = {"user_id": 1, "name": "太郎"}
+def create_user(user: User):
+    # レスポンスbody
+    return {"res": "ok", "ID": user.user_id, "名前": user.name}
+
+
+# ネストされたJSON Bodyの受け取り
+@app.post("/users/")
+# 上で定義したUserモデルのリクエストbodyをリストに入れた形で受け取る
+# users = [{"user_id": 1, "name": "太郎"},{"user_id": 2, "name": "次郎"}]
+def create_users(users: List[User]):
+    new_users = []
+    for user in users:
+        new_users.append({"res": "ok", "ID": user.user_id, "名前": user.name})
+    # 整形したデータをレスポンスbodyを送信
+    return new_users
